65ac7952e9c81a62b8cc180074975fa3daadb2d2
Verity <verity.boyd@edu.sait.ca>
2026-01-16 11:59:24 -0700

    page.js, siteheader and snacks components

---
+++ app/components/SiteHeader.js
@@ -0,0 +1,14 @@
+export default function SiteHeader() {
+  return (
+    <header className="flex gap-4 justify-between py-4 px-8">
+      <div>WDW vs Universal</div>
+      <nav>
+        <ul>
+          <li>Link 1</li>
+          <li>Link 2</li>
+          <li>Link 3</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
---
+++ app/components/Snacks.js
@@ -0,0 +1,22 @@
+export default function Snacks() {
+  return (
+    <section>
+      Need a parent tag
+      <header>
+        <h2 className="font-bold text-xl">Best Snacks</h2>
+      </header>
+      <div>
+        <header>
+          <h3 className="font-bold">Walt Disney World</h3>
+        </header>
+        <ul>
+          <li>Mickey Premium Ice Cream Bar — A classic fan favorite</li>
+          <li>Dole Whip — Refreshing pineapple soft-serve</li>
+          <li>Churros — Crispy with cinnamon sugar</li>
+          <li>School Bread (Norway Pavilion) — Sweet and delicious</li>
+          <li>Turkey Leg — Hearty and savory snack</li>
+        </ul>
+      </div>
+    </section>
+  );
+}
